9fa5948fc45254aa4b880af0fc4243e49e299109
Thien Su Nha Ben <dangpquoc2022@gmail.com>
2023-03-17 16:16:41 +0700

    changed structure format

---
+++ Class/map.py
@@ -0,0 +1,69 @@
+class O:
+    type = 4
+    def __init__(self, pos, targets):
+        self.pos = pos
+        self.targets = targets
+        
+class X:
+    type = 5
+    # example : pos = (1,2)  , targets = [ (1,1),(1,2),(1,3) ]
+    def __init__(self, pos, targets):
+        self.pos = pos
+        self.targets = targets
+
+class Split:
+    type = 6
+    def __init__(self, pos, block1, block2, target):
+        self.pos = pos
+        self.block1 = block1
+        self.block2 = block2
+        self.target = target
+    
+    
+    
+class Map:
+    def __init__(self , stageInfo):
+        with open(stageInfo, 'r') as f:
+            first_line = f.readline().strip()
+            first_line = [int(x) for x in first_line.split(" ")]
+            n = first_line[0]
+            
+            self.init_pos = (first_line[1],first_line[2])
+            
+            matrix = [f.readline().strip() for _ in range(n)]
+            self.matrix = [list(map(int, row.split(" "))) for row in matrix]
+            
+            for i in range(n):
+                for j in range(len(matrix[0])):
+                    if matrix[i][j] == 2:
+                        self.finish = (i,j)
+            
+            self.buttons = {}
+            buttons = [line.strip() for line in f]
+            for button in buttons:
+                x_pos = button[1]
+                y_pos = button[2]
+                button = [int(x) for x in button.split(" ")]
+                
+                type_button = self.matrix[button[0]][button[1]]
+                if type_button == 4:
+                    targets = []
+                    
+                    for i in range (2, len(button) , 2):
+                        targets.append((button[i], button[i+1]))
+                        
+                    x_button = X((x_pos,y_pos), targets)
+                    self.buttons[x_button.pos] = x_button
+                    
+                elif type_button == 5:
+                    targets = []
+                    
+                    for i in range (2, len(button) , 2):
+                        targets.append((button[i], button[i+1]))
+                        
+                    o_button = X((x_pos,y_pos), targets)
+                    self.buttons[o_button.pos] = o_button
+                    
+                elif type_button == 6:
+                    split_button = Split( (button[0],button[1]), (button[2],button[3]), (button[4],button[5]), (button[6],button[7]))
+                    self.buttons[split_button.pos] = split_button
---
+++ Class/state.py
@@ -0,0 +1,62 @@
+from Class.map import Map
+
+
+class State(Map):
+    def __init__(self,stageInfo):
+        super().__init__(stageInfo)
+        self.target_block = - 1  # -1 : None , 0 : first block , 1 : second block
+        # (first block pos , second block pos) : ((1,2),(3,4))
+        self.blocks = (self.init_pos, self.init_pos)
+
+    #"SPACE" action , no change in state
+    def switch_target(self):
+        if self.target_block == 0:
+            self.target_block = 1
+        elif self.target_block == 1:
+            self.target_block = 0
+
+    # return a next State() , do not update the self , action is "UP" , "DOWN" , "LEFT" , "RIGHT"
+    def next_state(self, action):
+        pass
+
+    # return true if game not over , false if game over for the current state (self)
+    def valid_state(self):
+        pass
+
+    # check if we triggered any type of button or join 2 block into 1 , if so update the state (self)
+    def trigger(self):
+        pass
+
+    @staticmethod
+    def repeated_state(state_log, new_state):
+        for state in reversed(state_log):
+            if new_state.matrix == state.matrix and new_state.blocks == state.blocks :
+                return True
+        return False
+    
+    #return all valid next state as much as possible : [("UP" , state1) ,("DOWN" , state2) ...]
+    def next_valid_states(self , state_log ):
+        res = []
+        if self.target_block == -1:
+            for action in ["UP" , "DOWN" , "LEFT" ,"RIGHT"]:
+                new_state = self.next_state(action)
+                if new_state.valid_state():
+                    new_state.trigger()
+                    if not new_state.repeated_state(state_log, new_state):
+                        res.append((action,new_state))
+        else:
+            for target in ["" , "SPACE "]:
+                for action in ["UP" , "DOWN" , "LEFT" ,"RIGHT"]:
+                    new_state = self
+                    if target == "SPACE " : 
+                        new_state.switch_target()
+                    new_state = new_state.next_state(action)
+                    if new_state.valid_state():
+                        new_state.trigger()
+                        if not new_state.repeated_state(state_log, new_state):
+                            res.append((target + action,new_state))
+        return res
+    def is_finished(self):
+        if self.blocks[0] == self.blocks[1] and self.blocks[0] == self.finish :
+            return True
+        return False
