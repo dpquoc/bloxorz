02b45df58e0402b3fe4d1c8b8de4c686146aa102
Nguyen <nguyen.nguyen942002@hcmut.edu.vn>
2023-03-23 17:16:02 +0700

    Update A_star.py (3rd)

--- Search/A_star.py
+++ Search/A_star.py
@@ -10,25 +10,30 @@ def A_star(initial_node):
     Node.close_list = []
     
     while len(Node.open_list) > 0:  
-        current_node = State.open_states[0]
+        if Node.open_list[0].repeated_node():
+            print( "[" + ' '.join(Node.open_list[0].get_action_list()) + " REPEATED]\n" )
+            Node.open_list.pop(0)
+            continue
+
+        current_node = Node.open_list[0]
         current_index = 0
         node_status = current_node.get_node_status()
 
         if node_status == 1 :
-            action_list = State.open_states[0].get_action_list()
+            action_list = Node.open_list[0].get_action_list()
             print( "[" + ' '.join(action_list) + " FINISH]" )
             return action_list
         elif node_status == -1 :
-            action_list = State.open_states[0].get_action_list()
+            action_list = Node.open_list[0].get_action_list()
             print( "[" + ' '.join(action_list) + " DEAD]" )
 
-        for index, item in enumerate(State.open_states):
+        for index, item in enumerate(Node.open_list):
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
 
-        State.open_states.pop(current_index)
-        State.close_states.append(current_node)
+        Node.open_list.pop(current_index)
+        Node.close_list.append(current_node)
 
         # if State.is_finished():
         #     path = []
@@ -42,7 +47,7 @@ def A_star(initial_node):
         children = current_node.add_childs()
 
         for child in children:
-            if child in State.close_states:
+            if child in Node.close_list:
                 continue
 
             child.g = current_node.g + 1
