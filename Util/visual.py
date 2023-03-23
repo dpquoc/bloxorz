353de389373a12c4bacb4abd9933106a9d2b8b5f
Hien Giang <quachtrieuvu@gmail.com>
2023-03-23 13:56:24 +0700

    add stage 11-17

--- Util/visual.py
+++ Util/visual.py
@@ -6,6 +6,7 @@ import pydirectinput
 import keyboard
 from PIL import Image
 
+
 class AutoGUI:
     def __init__(self, img_path, delay=1):
         self.img_path = img_path
@@ -19,22 +20,20 @@ class AutoGUI:
     def find_click(self):
         if self.image is not None:
             try:
-                center = pyautogui.locateCenterOnScreen(self.image, confidence=0.9)
+                center = pyautogui.locateCenterOnScreen(
+                    self.image, confidence=0.9)
                 if center is not None:
                     pyautogui.click(center)
                     time.sleep(self.delay)
                     return True
-                
+
             except:
                 pass
 
         return False
 
 
-
-
-
-def visual_output(actions, instage=False, passcode=None ):
+def visual_output(actions, instage=False, passcode=None):
     newgame = AutoGUI('./img/newgame.png', 1)
     skip = AutoGUI('./img/skip.png', 1)
     loadstage = AutoGUI('./img/loadstage.png', 1)
@@ -42,18 +41,18 @@ def visual_output(actions, instage=False, passcode=None ):
 
     try:
         while True:
-            if not instage :
-                if passcode==None:
+            if not instage:
+                if passcode == None:
                     if newgame.find_click():
                         skip.find_click()
-                else:          
+                else:
                     if loadstage.find_click():
                         if enterpasscode.find_click():
                             pyautogui.typewrite(passcode)
-                            instage=True
+                            instage = True
                             time.sleep(6)
             else:
-                if pyautogui.locateCenterOnScreen('./img/menu.png' , confidence=0.9) != None :
+                if pyautogui.locateCenterOnScreen('./img/menu.png', confidence=0.9) != None:
                     for action in actions:
                         pydirectinput.press(action.lower())
                         time.sleep(0.3)
